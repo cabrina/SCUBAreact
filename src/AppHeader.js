0b1219d404b3e4bca3d470bc3d840f9f51f544fd
Cabrina <cabrinaengedal@outlook.com>
2019-04-03 10:49:14 +0200

    finished header and added imgs's

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -5,6 +5,7 @@ class Appheader extends Component {
     render(){
         return(
             <header>
+                <img src="../img/ScubaLiving_logo_small.svg" alt="scubaliving logo"/>
                 <nav>
                     <ul>
                         <li><Link to="/">HOME</Link></li>
@@ -15,6 +16,14 @@ class Appheader extends Component {
                         <li><Link to="/about">OM OS</Link></li>
                     </ul>
                 </nav>
+                <form action="#">
+                    <input type="text" className="search"/>
+                    <input type="button" value="SØG" className="button"/>
+                </form>
+                <div className="login">
+                    <img src="../img/lock-icon-300x300.png" alt="login"/>
+                    <p>LOGIN</p>
+                </div>
             </header>
         )
     }
